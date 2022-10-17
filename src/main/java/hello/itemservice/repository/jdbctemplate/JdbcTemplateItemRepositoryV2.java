2f9f1ec356c3b55ed1e066a459e41bc496faaa2c
ywj9811 <ywj9811@naver.com>
2022-10-18 02:48:08 +0900

    코드 정리

--- src/main/java/hello/itemservice/repository/jdbctemplate/JdbcTemplateItemRepositoryV2.java
+++ src/main/java/hello/itemservice/repository/jdbctemplate/JdbcTemplateItemRepositoryV2.java
@@ -32,10 +32,13 @@ import java.util.Optional;
 @Slf4j
 @Repository
 public class JdbcTemplateItemRepositoryV2 implements ItemRepository {
+
     private final NamedParameterJdbcTemplate template;
+
     public JdbcTemplateItemRepositoryV2(DataSource dataSource) {
         this.template = new NamedParameterJdbcTemplate(dataSource);
     }
+
     @Override
     public Item save(Item item) {
         String sql = "insert into item (item_name, price, quantity) " +
@@ -47,6 +50,7 @@ public class JdbcTemplateItemRepositoryV2 implements ItemRepository {
         item.setId(key);
         return item;
     }
+
     @Override
     public void update(Long itemId, ItemUpdateDto updateParam) {
         String sql = "update item " +
@@ -64,6 +68,7 @@ public class JdbcTemplateItemRepositoryV2 implements ItemRepository {
                 .addValue("id", itemId); //이 부분이 별도로 필요하다.
         template.update(sql, param);
     }
+
     @Override
     public Optional<Item> findById(Long id) {
         String sql = "select id, item_name, price, quantity from item where id = :id";
@@ -75,35 +80,45 @@ public class JdbcTemplateItemRepositoryV2 implements ItemRepository {
             return Optional.empty();
         }
     }
+
     @Override
     public List<Item> findAll(ItemSearchCond cond) {
         Integer maxPrice = cond.getMaxPrice();
         String itemName = cond.getItemName();
         SqlParameterSource param = new BeanPropertySqlParameterSource(cond);
+
         String sql = "select id, item_name, price, quantity from item";
+
+
         //동적 쿼리
         if (StringUtils.hasText(itemName) || maxPrice != null) {
             sql += " where";
         }
+
         boolean andFlag = false;
+
         if (StringUtils.hasText(itemName)) {
             sql += " item_name like concat('%',:itemName,'%')";
             andFlag = true;
         }
+
         if (maxPrice != null) {
             if (andFlag) {
                 sql += " and";
             }
             sql += " price <= :maxPrice";
         }
+
         log.info("sql={}", sql);
         return template.query(sql, param, itemRowMapper());
     }
+
     private RowMapper<Item> itemRowMapper() {
         return BeanPropertyRowMapper.newInstance(Item.class); //camel 변환 지원
-    }
-}
         /**
          * 데이터베이스에서 조회한 결과 이름을 기반으로 setId(), setPrice() 등등을 실행해줌
          */
+    }
+}
+
 
