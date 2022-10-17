a62591c665efa35b3f727fc805415fdaef6e8cc6
ywj9811 <ywj9811@naver.com>
2022-10-18 02:54:33 +0900

    코드 정리

--- src/main/java/hello/itemservice/repository/jdbctemplate/JdbcTemplateItemRepositoryV2.java
+++ src/main/java/hello/itemservice/repository/jdbctemplate/JdbcTemplateItemRepositoryV2.java
@@ -41,8 +41,7 @@ public class JdbcTemplateItemRepositoryV2 implements ItemRepository {
 
     @Override
     public Item save(Item item) {
-        String sql = "insert into item (item_name, price, quantity) " +
-                "values (:itemName, :price, :quantity)";
+        String sql = "insert into item (item_name, price, quantity) values (:itemName, :price, :quantity)";
         SqlParameterSource param = new BeanPropertySqlParameterSource(item);
         KeyHolder keyHolder = new GeneratedKeyHolder();
         template.update(sql, param, keyHolder);
