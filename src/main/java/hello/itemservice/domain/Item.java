c94e5aa13130584c53795b55d7a145097af69d00
양원준 <97458548+ywj9811@users.noreply.github.com>
2022-12-05 23:13:26 +0900

    feat: Entity 설정

--- src/main/java/hello/itemservice/domain/Item.java
+++ src/main/java/hello/itemservice/domain/Item.java
@@ -2,16 +2,28 @@ package hello.itemservice.domain;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
 @Data
+@Entity
 public class Item {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "item_name", length = 10)
     private String itemName;
+    /**
+     * 사실 itemName의 경우 item_name과
+     * 자동으로 매칭이 되기 때문에 @Column은 생략해도 상관없다.
+     */
     private Integer price;
     private Integer quantity;
 
     public Item() {
+        /**
+         * JPA는 기본 생성자가 필수이다 (public 혹은 protected)
+         */
     }
 
     public Item(String itemName, Integer price, Integer quantity) {
