f55e3aada01a1cea19e0aa3f4bb457ba39d724c1
양원준 <97458548+ywj9811@users.noreply.github.com>
2022-12-07 00:01:42 +0900

    feat: SpringDataJpa 사용 설정

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -11,7 +11,8 @@ import org.springframework.context.annotation.Profile;
 
 @Slf4j
 //@Import(MyBatisConfig.class)
-@Import(JpaConfig.class)
+//@Import(JpaConfig.class)
+@Import(SpringDataJpaConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web") //componentscan 범위 설정
 public class ItemServiceApplication {
 
