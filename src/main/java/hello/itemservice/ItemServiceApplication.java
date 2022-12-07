5fd121e8524f12bb7074671e1719e10530b0e825
양원준 <97458548+ywj9811@users.noreply.github.com>
2022-12-07 22:09:48 +0900

    feat: Querydsl 사용 설정 추가 및 메인에 추가

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -12,7 +12,8 @@ import org.springframework.context.annotation.Profile;
 @Slf4j
 //@Import(MyBatisConfig.class)
 //@Import(JpaConfig.class)
-@Import(SpringDataJpaConfig.class)
+//@Import(SpringDataJpaConfig.class)
+@Import(QuerydslConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web") //componentscan 범위 설정
 public class ItemServiceApplication {
 
